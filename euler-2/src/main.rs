e390c1bf5477ef86eeb352cbc53d6cc09e9ddab9
smsutherland <sagan@farthinghalearms.com>
2024-05-05 16:45:25 -0400

    problem 2

---
+++ euler-2/src/main.rs
@@ -0,0 +1,17 @@
+fn main() {
+    let mut sum: u64 = 0;
+    let mut a = 1;
+    let mut b = 1;
+    loop {
+        if b > 4000000 {
+            break;
+        }
+        let c = a + b;
+        if c % 2 == 0 {
+            sum += c;
+        }
+        a = b;
+        b = c;
+    }
+    println!("{sum}");
+}
