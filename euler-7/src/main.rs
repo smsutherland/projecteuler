313dce67f19e62e9a17d554d86a2c38a4e065971
smsutherland <sagan@farthinghalearms.com>
2024-05-05 21:08:27 -0400

    prime finders in util

--- euler-7/src/main.rs
+++ euler-7/src/main.rs
@@ -1,18 +1,6 @@
+use euler_util::n_primes;
+
 fn main() {
-    const LIM: usize = 10001;
-    let mut primes = vec![2];
-    let mut count = 3;
-    while primes.len() < LIM {
-        'block: {
-            for p in &primes {
-                if count % p == 0 {
-                    count += 1;
-                    break 'block;
-                }
-            }
-            primes.push(count);
-            count += 2;
-        }
-    }
+    let primes = n_primes(10001);
     println!("{}", primes.last().unwrap());
 }
--- euler-util/src/lib.rs
+++ euler-util/src/lib.rs
@@ -15,3 +15,38 @@ pub fn factor(num: u64) -> Vec<u64> {
     factors
 }
 
+pub fn n_primes(n: usize) -> Vec<u64> {
+    let mut primes = vec![2];
+    let mut count = 3;
+    while primes.len() < n {
+        'block: {
+            for p in &primes {
+                if count % p == 0 {
+                    count += 1;
+                    break 'block;
+                }
+            }
+            primes.push(count);
+            count += 2;
+        }
+    }
+    primes
+}
+
+pub fn primes_under(lim: u64) -> Vec<u64> {
+    let mut primes = vec![2];
+    let mut count = 3;
+    while *primes.last().unwrap() < lim {
+        'block: {
+            for p in &primes {
+                if count % p == 0 {
+                    count += 1;
+                    break 'block;
+                }
+            }
+            primes.push(count);
+            count += 2;
+        }
+    }
+    primes
+}
