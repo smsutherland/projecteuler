1f6f12d87e52914ef4524594831b92a926744a14
smsutherland <sagan@farthinghalearms.com>
2024-05-06 19:47:20 -0400

    problem 15

---
+++ euler-15/src/main.rs
@@ -0,0 +1,20 @@
+fn main() {
+    const WIDTH: usize = 20;
+    const HEIGHT: usize = 20;
+
+    let mut paths: [[usize; WIDTH + 1]; HEIGHT + 1] = Default::default();
+    for y in 0..=HEIGHT {
+        for x in 0..=WIDTH {
+            let mut path = 0;
+            if x != 0 {
+                path += paths[y][x - 1];
+            }
+            if y != 0 {
+                path += paths[y - 1][x];
+            }
+            paths[y][x] = path.max(1);
+        }
+    }
+
+    println!("{}", paths[HEIGHT][WIDTH]);
+}
