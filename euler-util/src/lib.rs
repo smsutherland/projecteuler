5041ed3d7d5eb7ac3231813fdf430e382497c3e9
smsutherland <sagan@farthinghalearms.com>
2024-05-05 20:04:27 -0400

    extract util lib

---
+++ euler-util/src/lib.rs
@@ -0,0 +1,17 @@
+pub fn factor(num: u64) -> Vec<u64> {
+    let mut factors = Vec::new();
+    let mut n = num;
+    let mut i = 2;
+    while i <= n {
+        if n % i == 0 {
+            factors.push(i);
+            n /= i;
+        } else if i == 2 {
+            i += 1;
+        } else {
+            i += 2;
+        }
+    }
+    factors
+}
+
