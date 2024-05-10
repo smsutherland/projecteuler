b0b7c2c8e84f02187f13393c10e2dcf3bb907765
smsutherland <sagan@farthinghalearms.com>
2024-05-10 15:49:49 -0400

    problem 22

---
+++ euler-22/src/main.rs
@@ -0,0 +1,13 @@
+const NAMES_RAW: &str = include_str!("../data/0022_names.txt");
+
+fn name_score((i, s): (usize, &str)) -> u64 {
+    let s: u64 = s.as_bytes().iter().map(|c| (c - (b'A' - 1)) as u64).sum();
+    s * (i + 1) as u64
+}
+
+fn main() {
+    let mut names: Vec<_> = NAMES_RAW.split(',').map(|s| &s[1..s.len() - 1]).collect();
+    names.sort_unstable();
+    let scores: u64 = names.iter().copied().enumerate().map(name_score).sum();
+    println!("{scores}");
+}
