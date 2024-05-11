4a21a5a223678343a819da378fd8d586086c8828
smsutherland <sagan@farthinghalearms.com>
2024-05-10 20:04:52 -0400

    problem 26

---
+++ euler-26/src/main.rs
@@ -0,0 +1,37 @@
+const MAX: u32 = 1000;
+
+fn run() -> u32 {
+    let mut max = 0;
+    let mut max_val = 0;
+    for i in 1..MAX {
+        // perform long division.
+        let mut remainder = 1;
+        let mut states = Vec::new();
+        let repeat_len = loop {
+            remainder *= 10;
+            // let digit = remainder / i;
+            remainder %= i;
+            if let Some(n) = states.iter().position(|&x| x == remainder) {
+                break states.len() - n;
+            }
+            states.push(remainder);
+            if remainder == 0 {
+                break 0;
+            }
+        };
+        if repeat_len > max {
+            max = repeat_len;
+            max_val = i;
+        }
+    }
+    max_val
+}
+
+fn main() {
+    println!("{}", run());
+}
+
+#[test]
+fn euler_26() {
+    assert_eq!(run(), 983);
+}
